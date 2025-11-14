f90049b3c488e58dc375eac7ebd4ac0ad59ca475
SebD111 <sed006@edu.zealand.dk>
2025-11-14 12:26:23 +0100

    added branch

---
+++ GitHubGruppeTest/TestBranchClass_Sebastian.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubGruppeTest
+{
+    public class TestBranchClass_Sebastian
+    {
+
+        public TestBranchClass_Sebastian(string name) 
+        {
+            Name = name;
+        
+        }
+
+        public string Name 
+        {get; set;}
+
+        public string ToString()
+        {
+            return $"Name: {Name}";
+        }
+    }
+}
