6e0e5913689818329d4174189788c09c99e8381f
Sebahattin Emre Türkoglu <set003@edu.zealand.dk>
2025-11-20 11:06:31 +0100

    Added Test class, and added Age to test class

---
+++ GitHubGruppeTest/TestBranchClass_Sebahattin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubGruppeTest
+{
+    public class TestBranchClass_Sebahattin
+    {
+        public TestBranchClass_Sebahattin(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name
+        { get; set; }
+
+        public int Age { get; set; }
+        public string ToString()
+        {
+            return $"Name: {Name} - {Age}";
+        }
+    }
+}
